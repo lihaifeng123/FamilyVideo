311acb83c0e71bf0f21df5f1aea04ba335c3db10
lihaifeng <aytlhf@qq.com>
2018-05-17 13:08:23 +0800

    项目配置创建

---
+++ FamilyDataCenter/MyVideoApp/MyVideoApp/Util/AppConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVideoApp.Util
+{
+    public static class AppConfig
+    {
+        public static string AppVersion = "0.1";
+
+        public static string AppName = "家庭视频监控App";
+
+        public static string BaseDb = "";
+    }
+}
