3069f63e2391d9a3d3d3a0ff55d67241ab9f4b64
lihaifeng <aytlhf@qq.com>
2018-05-18 12:08:58 +0800

    登陆用户实体创建

---
+++ FamilyDataCenter/MyVideoApp/MyVideoApp/Models/UserModels/LoginUser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVideoApp.Models.UserModels
+{
+    public class LoginUser
+    {
+        private string Name { get; set; }
+
+        private string Password
+        {
+            set;
+            get;
+        }
+
+        public static bool operator ==(LoginUser user1,LoginUser user2)
+        {
+            return user1.Equals(user2);
+        }
+        public static bool operator !=(LoginUser user1, LoginUser user2)
+        {
+            return !user1.Equals(user2);
+        }
+        public override bool Equals(Object obj)
+        {
+            try
+            {
+                LoginUser login = (LoginUser)obj;
+                if (login.Name == this.Name && login.Password == this.Password)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }catch(Exception e)
+            {
+                return false;
+            }
+            
+        }
+    }
+}
